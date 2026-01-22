8f83920dc15f8061511ececfa0104ddf85e51af1
animallibraryn4 <nikhil7858978052@gmail.com>
2026-01-22 13:57:04 +0530

    Create __init__.py

---
+++ plugins/__init__.py
@@ -0,0 +1,6 @@
+# plugins/__init__.py
+from . import (
+    admin, broadcast, callback, caption, cb_data, 
+    filedetect, metadata, myplane, ping, restart, 
+    start, thumbfunction, totalusers, upgrade
+)
